b356cc0a148d04cea5a761bfb6ee5c5d801912bf
miketoryan <ryan.only@outlook.com>
2026-09-22 11:48:31 +0800

    fix: support pre-iOS17 keyboard URL launcher

--- TypeVoiceKeyboard/KeyboardViewController.swift
+++ TypeVoiceKeyboard/KeyboardViewController.swift
@@ -22,7 +22,7 @@ private struct TypeVoiceURLLauncherView: View {
     var body: some View {
         Color.clear
             .frame(width: 1, height: 1)
-            .onChange(of: launcher.request) { _, request in
+            .onChange(of: launcher.request) { request in
                 guard let request else { return }
                 openURL(request.url)
                 launcher.request = nil
